3a5c1d0a65694e76d1ec3eaea369b23bccd5dc59
JulieUniverse <universejulie@gmail.com>
2018-10-17 21:57:21 -0700

    First commit

---
+++ GameMaster.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+    public AudioClip killPigAudio;
+    private AudioSource source;
+    public static GameMaster gm;
+    public Transform player;
+    public Transform spawnPoint;
+    private Health health;
+    public GameObject gameOverScreen;
+    public bool death;
+    private Poop poopScript;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    void Awake()
+    {
+        health = FindObjectOfType<Health>();
+        poopScript = FindObjectOfType<Poop>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            KillPlayer();
+            //poopScript.SelfDestruct();
+            RespawnPlayer();
+            GameOverOn();
+            //Restart();
+            //Invoke("Restart",1f);
+        }
+        //death = false;
+    }
+
+    void KillPlayer()
+    {
+        death = true;
+        source.PlayOneShot(killPigAudio);
+        health.lives -= 1;
+        Debug.Log(health.lives);
+    }
+    void RespawnPlayer() {
+        //death = false;
+        player.transform.position = new Vector2(spawnPoint.position.x, spawnPoint.position.y);
+    }
+
+    void Restart()
+    {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOverOn()
+    {
+        if (health.lives <= 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").SetActive(false);
+            gameOverScreen.SetActive(true);
+        }
+
+    }
+}
---
+++ Poop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poop : MonoBehaviour {
+
+    public CircleCollider2D stinkRadius;
+    public BoxCollider2D poopCollider;
+    public GameMaster gm;
+    private GameObject poop;
+
+
+    void Awake()
+    {
+        gm = FindObjectOfType<GameMaster>();
+        poop = GameObject.FindWithTag("Poop");
+    }
+    void FixedUpdate()
+    {
+        SelfDestruct();
+    }
+    public void SelfDestruct()
+    {
+        if(gm.death == true)
+        GameObject.Destroy(poop);
+    }
+}
